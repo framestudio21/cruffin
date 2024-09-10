79bd990773e9d74103028e68c490d2e1b86c81a1
framestudio21 <info.framestudio21@gmail.com>
2024-09-10 21:10:34 +0530

    update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 import Styles from "../styles/login.module.css"
 
-import NavBar from './component/navbar'
 
 export default function login() {
   return (
     <>
-    <NavBar/>
     <div className={Styles.mainbody}>
         <div className={Styles.loginbody}>
             a
